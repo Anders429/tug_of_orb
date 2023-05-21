ce511337eb1504f819de2efa447046a361b01064
Anders429 <anders.stefan.evensen@gmail.com>
2023-05-21 13:42:25 -0700

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[cfg(debug_assertions)]
+use ::{
+    core::fmt::Write,
+    log::error,
+    mgba_log::{
+        MgbaLogLevel,
+        MgbaWriter,
+    }
+};
+use gba::interrupts::IrqBits;
+
+/// This panic handler is specifically for debug mode.
+/// 
+/// When panicking in debug builds, the panic info is logged as an error. Following this, a fatal
+/// log will occur to halt emulation and display an error to the user. This is done to ensure the
+/// entirety of the panic info is displayed to the user, as mGBA only allows for up to 256 bytes to
+/// be logged at once.
+#[cfg(debug_assertions)]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    error!("{}", info);
+    MgbaWriter::new(MgbaLogLevel::Fatal)
+        .write_str("Halting due to panic. See logs for `PanicInfo`.");
+    loop {}
+}
+
+/// This panic handler is specifically for release mode.
+/// 
+/// In release builds, panicking just causes the game to lock up. Ideally, panicking should not
+/// occur at all in release builds.
+#[cfg(not(debug_assertions))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[link_section = ".iwram"]
+extern "C" fn irq_handler(_: IrqBits) {}
+
+/// Entry point for the game.
+#[no_mangle]
+extern "C" fn main() -> ! {
+    // Initialize the global logger.
+    //
+    // This logging only works in mGBA. It is only enabled in debug builds.
+    #[cfg(debug_assertions)]
+    mgba_log::init().expect("failed to initialize mgba logging");
+
+    log::info!("Hello, world!");
+
+    loop {}
 }
