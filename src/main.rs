9c7a03f7fd2b61bd26edfa4361149839049658ff
Anders429 <anders.stefan.evensen@gmail.com>
2023-05-22 22:51:25 -0700

    Draw a cursor.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,14 @@ use game::{Direction, Game, Position};
 use gba::{
     bios::VBlankIntrWait,
     interrupts::IrqBits,
-    mmio::{BG0CNT, DISPCNT, DISPSTAT, IE, IME, KEYINPUT},
-    video::{BackgroundControl, DisplayControl, DisplayStatus, VideoMode},
+    mmio::{
+        obj_palbank, BG0CNT, DISPCNT, DISPSTAT, IE, IME, KEYINPUT, OBJ_ATTR0, OBJ_ATTR_ALL,
+        OBJ_TILES,
+    },
+    video::{
+        obj::{ObjAttr, ObjAttr0, ObjDisplayStyle},
+        BackgroundControl, Color, DisplayControl, DisplayStatus, VideoMode,
+    },
 };
 #[cfg(debug_assertions)]
 use ::{
@@ -69,8 +75,9 @@ extern "C" fn main() -> ! {
     // Set BG0 to be displayed.
     DISPCNT.write(
         DisplayControl::new()
-            .with_video_mode(VideoMode::_2)
-            .with_show_bg0(true),
+            .with_show_bg0(true)
+            .with_show_obj(true)
+            .with_obj_vram_1d(true),
     );
 
     let mut state = State {
@@ -79,6 +86,70 @@ extern "C" fn main() -> ! {
         cursor: Position { x: 0, y: 0 },
     };
     log::info!("{:?}", state);
+
+    // Define a palette.
+    let colors = [Color::from_rgb(31, 16, 1), Color::from_rgb(3, 3, 3)];
+    for (index, color) in colors.iter().enumerate() {
+        obj_palbank(0).index(index + 1).write(*color);
+    }
+
+    // Define the tiles.
+    //
+    // There has got to be some better way. Need to figure out how to convert resources to 4bpp.
+    OBJ_TILES.as_region().get(0).unwrap().write([
+        0b0000_0000_0010_0010_0010_0010_0010_0010,
+        0b0000_0000_0010_0001_0001_0001_0001_0010,
+        0b0000_0000_0010_0010_0010_0001_0001_0010,
+        0b0000_0000_0000_0000_0010_0010_0001_0010,
+        0b0000_0000_0000_0000_0000_0010_0001_0010,
+        0b0000_0000_0000_0000_0000_0010_0010_0010,
+        0b0000_0000_0000_0000_0000_0000_0000_0000,
+        0b0000_0000_0000_0000_0000_0000_0000_0000,
+    ]);
+    OBJ_TILES.as_region().get(1).unwrap().write([
+        0b0010_0010_0010_0010_0010_0010_0000_0000,
+        0b0010_0001_0001_0001_0001_0010_0000_0000,
+        0b0010_0001_0001_0010_0010_0010_0000_0000,
+        0b0010_0001_0010_0010_0000_0000_0000_0000,
+        0b0010_0001_0010_0000_0000_0000_0000_0000,
+        0b0010_0010_0010_0000_0000_0000_0000_0000,
+        0b0000_0000_0000_0000_0000_0000_0000_0000,
+        0b0000_0000_0000_0000_0000_0000_0000_0000,
+    ]);
+    OBJ_TILES.as_region().get(2).unwrap().write([
+        0b0000_0000_0000_0000_0000_0000_0000_0000,
+        0b0000_0000_0000_0000_0000_0000_0000_0000,
+        0b0000_0000_0000_0000_0000_0010_0010_0010,
+        0b0000_0000_0000_0000_0000_0010_0001_0010,
+        0b0000_0000_0000_0000_0010_0010_0001_0010,
+        0b0000_0000_0010_0010_0010_0001_0001_0010,
+        0b0000_0000_0010_0001_0001_0001_0001_0010,
+        0b0000_0000_0010_0010_0010_0010_0010_0010,
+    ]);
+    OBJ_TILES.as_region().get(3).unwrap().write([
+        0b0000_0000_0000_0000_0000_0000_0000_0000,
+        0b0000_0000_0000_0000_0000_0000_0000_0000,
+        0b0010_0010_0010_0000_0000_0000_0000_0000,
+        0b0010_0001_0010_0000_0000_0000_0000_0000,
+        0b0010_0001_0010_0010_0000_0000_0000_0000,
+        0b0010_0001_0001_0010_0010_0010_0000_0000,
+        0b0010_0001_0001_0001_0001_0010_0000_0000,
+        0b0010_0010_0010_0010_0010_0010_0000_0000,
+    ]);
+
+    // Define the object.
+    let mut obj = ObjAttr::new();
+    obj.set_x(44);
+    obj.set_y(38);
+    obj.set_tile_id(0);
+    obj.set_palbank(0);
+    obj.1 = obj.1.with_size(1);
+    OBJ_ATTR_ALL.get(0).unwrap().write(obj);
+
+    // Hide other objects.
+    OBJ_ATTR0.iter().skip(1).for_each(|address| {
+        address.write(ObjAttr0::new().with_style(ObjDisplayStyle::NotDisplayed))
+    });
 
     loop {
         // Read keys for each frame.
@@ -103,11 +174,6 @@ extern "C" fn main() -> ! {
 
         VBlankIntrWait();
 
-        // Draw the grid.
-        // for row in &board.grid {
-        //     for square in row {
-
-        //     }
-        // }
+        // Draw the cursor.
     }
 }
