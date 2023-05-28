a9812d2fe1e3e330cb6aa328fd08f29a031d4ce0
Anders429 <anders.stefan.evensen@gmail.com>
2023-05-28 00:17:28 -0700

    Title screen.

--- src/screen/title.rs
+++ src/screen/title.rs
@@ -7,7 +7,7 @@ use core::slice;
 use gba::{
     bios::VBlankIntrWait,
     mmio::{
-        bg_palbank, BG2CNT, BG_PALETTE, BLDCNT, BLDY, CHARBLOCK0_8BPP, CHARBLOCK1_8BPP,
+        bg_palbank, BG1CNT, BG2CNT, BG_PALETTE, BLDCNT, BLDY, CHARBLOCK0_4BPP, CHARBLOCK1_4BPP,
         CHARBLOCK2_8BPP, DISPCNT, KEYINPUT, TEXT_SCREENBLOCKS,
     },
     video::{
@@ -36,45 +36,106 @@ impl Title {
         // Fade out while we set up the screen.
         BLDY.write(16);
 
-        BG2CNT.write(BackgroundControl::new().with_screenblock(8).with_bpp8(true));
-        DISPCNT.write(DisplayControl::new().with_show_bg2(true));
+        BG1CNT.write(
+            BackgroundControl::new()
+                .with_screenblock(8)
+                .with_priority(1),
+        );
+        BG2CNT.write(
+            BackgroundControl::new()
+                .with_screenblock(16)
+                .with_priority(0),
+        );
+        DISPCNT.write(
+            DisplayControl::new()
+                .with_show_bg1(true)
+                .with_show_bg2(true),
+        );
 
         // Load palette.
-        for (index, bytes) in Align4(*include_bytes!("../../res/splash_jam.pal"))
+        for (index, bytes) in Align4(*include_bytes!("../../res/title.pal"))
             .as_u16_slice()
             .iter()
             .enumerate()
         {
-            BG_PALETTE
+            bg_palbank(0)
                 .get(index)
                 .unwrap()
                 .write(gba::video::Color(*bytes));
         }
-
-        // Load tiles.
-        let aligned_bytes = Align4(*include_bytes!("../../res/splash_jam.8bpp"));
-        let bytes = aligned_bytes.as_u32_slice();
-        let len = bytes.len() / 16;
-        let tiles = unsafe { slice::from_raw_parts(bytes.as_ptr() as *const [u32; 16], len) };
-        CHARBLOCK0_8BPP
-            .as_region()
-            .sub_slice(0..len)
-            .write_from_slice(&tiles[0..len]);
-
-        // Draw the logo.
-        for (index, tile) in Align4(*include_bytes!("../../res/splash_jam.map"))
+        // Load red palette.
+        for (index, bytes) in Align4(*include_bytes!("../../res/red.pal"))
             .as_u16_slice()
             .iter()
             .enumerate()
         {
-            let x = index % 30;
-            let y = index / 30;
+            bg_palbank(1).index(index).write(gba::video::Color(*bytes));
+        }
+
+        // Load tiles.
+        let aligned_bytes = Align4(*include_bytes!("../../res/title.4bpp"));
+        let bytes = aligned_bytes.as_u32_slice();
+        let len = bytes.len() / 8;
+        let tiles = unsafe { slice::from_raw_parts(bytes.as_ptr() as *const [u32; 8], len) };
+        CHARBLOCK0_4BPP
+            .as_region()
+            .sub_slice(0..len)
+            .write_from_slice(&tiles[0..len]);
+
+        let aligned_bytes = Align4(*include_bytes!("../../res/background.4bpp"));
+        let bytes = aligned_bytes.as_u32_slice();
+        let len = bytes.len() / 8;
+        let tiles = unsafe { slice::from_raw_parts(bytes.as_ptr() as *const [u32; 8], len) };
+        CHARBLOCK0_4BPP
+            .as_region()
+            .sub_slice(75..75 + len)
+            .write_from_slice(&tiles[0..len]);
+
+        let aligned_bytes = Align4(*include_bytes!("../../res/empty.4bpp"));
+        let bytes = aligned_bytes.as_u32_slice();
+        let len = bytes.len() / 8;
+        let tiles = unsafe { slice::from_raw_parts(bytes.as_ptr() as *const [u32; 8], len) };
+        CHARBLOCK0_4BPP
+            .as_region()
+            .sub_slice(76..76 + len)
+            .write_from_slice(&tiles[0..len]);
+
+        // Draw white background.
+        for y in 0..20 {
+            for x in 0..30 {
+                TEXT_SCREENBLOCKS
+                    .get_frame(8)
+                    .unwrap()
+                    .get_row(y)
+                    .unwrap()
+                    .get(x)
+                    .unwrap()
+                    .write(TextEntry::new().with_tile(75).with_palbank(1));
+                TEXT_SCREENBLOCKS
+                    .get_frame(16)
+                    .unwrap()
+                    .get_row(y)
+                    .unwrap()
+                    .get(x)
+                    .unwrap()
+                    .write(TextEntry::new().with_tile(76).with_palbank(0));
+            }
+        }
+
+        // Draw the logo.
+        for (index, tile) in Align4(*include_bytes!("../../res/title.map"))
+            .as_u16_slice()
+            .iter()
+            .enumerate()
+        {
+            let x = index % 10;
+            let y = index / 10;
             TEXT_SCREENBLOCKS
-                .get_frame(8)
+                .get_frame(16)
                 .unwrap()
                 .get_row(y)
                 .unwrap()
-                .get(x)
+                .get(x + 10)
                 .unwrap()
                 .write(TextEntry::new().with_tile(*tile));
         }
