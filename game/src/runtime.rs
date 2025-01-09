0f15a860cfa1b91b47a5c9e6b963e9f568134841
Anders429 <anders.stefan.evensen@gmail.com>
2025-01-08 19:33:06 -0700

    Normalize the comments in the runtime.

--- game/src/runtime.rs
+++ game/src/runtime.rs
@@ -15,14 +15,14 @@ global_asm! {
     ".code 32",
 
     "b 1f",
-    ".space 0xE0", /* space for header */
-    "1:", /* post header */
+    ".space 0xE0", // Space for header.
+    "1:",
     "mov r12, #{mmio_base}",
     "add r0, r12, #{waitcnt_offset}",
     "ldr r1, ={waitcnt_setting}",
     "strh r1, [r0]",
 
-    /* iwram copy */
+    // iwram copy
     "ldr r4, =__iwram_word_copy_count",
     "cmp r4, #0",
     "beq 2 f",
@@ -30,13 +30,13 @@ global_asm! {
     "mov r5, #{dma3_setting}",
     "ldr r0, =__iwram_start",
     "ldr r2, =__iwram_position_in_rom",
-    "str r2, [r3]", /* source */
-    "str r0, [r3, #4]", /* destination */
-    "strh r4, [r3, #8]", /* word count */
-    "strh r5, [r3, #10]", /* set control bits */
+    "str r2, [r3]", // source
+    "str r0, [r3, #4]", // destination
+    "strh r4, [r3, #8]", // word count
+    "strh r5, [r3, #10]", // set control bits
     "2:",
 
-    /* ewram copy */
+    // ewram copy
     "ldr r4, =__ewram_word_copy_count",
     "cmp r4, #0",
     "beq 3 f",
@@ -44,13 +44,13 @@ global_asm! {
     "mov r5, #{dma3_setting}",
     "ldr r0, =__ewram_start",
     "ldr r2, =__ewram_position_in_rom",
-    "str r2, [r3]", /* source */
-    "str r0, [r3, #4]", /* destination */
-    "strh r4, [r3, #8]", /* word count */
-    "strh r5, [r3, #10]", /* set control bits */
+    "str r2, [r3]", // source
+    "str r0, [r3, #4]", // destination
+    "strh r4, [r3, #8]", // word count
+    "strh r5, [r3, #10]", // set control bits
     "3:",
 
-    /* bss zero */
+    // bss zero
     "ldr r4, =__bss_word_clear_count",
     "cmp r4, #0",
     "beq 4 f",
@@ -62,21 +62,21 @@ global_asm! {
     "bne 2b",
     "4:",
 
-    /* assign the runtime irq handler */
+    // assign the runtime irq handler
     "ldr r1, =__runtime_irq_handler",
     "str r1, [r12, #-4]",
 
-        /* call to rust main */
+    // call to main function
     "ldr r0, =main",
     "bx r0",
-    // main shouldn't return, but if it does just SoftReset
+    // main shouldn't return, but if it does just soft reset
     "swi #0",
 
     ".code 16",
 
     mmio_base = const MMIO_BASE,
     waitcnt_offset = const WAITCNT_OFFSET,
-    waitcnt_setting = const 0x4317 /*sram8,r0:3.1,r1:4.2,r2:8.2,no_phi,prefetch*/,
+    waitcnt_setting = const 0x4317, // sram8,r0:3.1,r1:4.2,r2:8.2,no_phi,prefetch
     dma3_offset = const DMA3_OFFSET,
     dma3_setting = const DMA_32_BIT_MEMCPY.to_u16(),
 }
@@ -88,23 +88,23 @@ global_asm! {
 
     ".code 32",
 
-    /* swap IME off, user can turn it back on if they want */
+    // swap IME off, user can turn it back on if they want
     "add r12, r0, #{ime_offset}",
     "mov r3, #0",
     "swp r3, r3, [r12]",
 
-    /* Read/Update IE and IF */
+    // Read/Update IE and IF
     "ldr r0, [r12, #-8]",
     "and r0, r0, r0, LSR #16",
     "strh r0, [r12, #-6]",
 
-    /* Read/Update BIOS_IF */
+    // Read/Update BIOS_IF
     "sub  r2, r12, #(0x208+8)",
     "ldrh r1, [r2]",
     "orr  r1, r1, r0",
     "strh r1, [r2]",
 
-    /* Restore initial IME setting and return */
+    // Restore initial IME setting and return
     "swp r3, r3, [r12]",
     "bx lr",
 
